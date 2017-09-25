c6b0784a32cdd5e4cd453873f1c06361bd616d2a
ympneki <equation_y_39@yahoo.co.jp>
2017-09-25 16:05:02 +0900

    test

---
+++ sample.java
@@ -0,0 +1,26 @@
+package practice;
+
+public class sample {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello, world!!");
+
+		loop();
+	}
+
+	public static void loop() {
+
+		int i;
+
+		for(i = 0; i < 10; i++) {
+			System.out.println("unko");
+		}
+		if(i == 10) {
+			while(true) {
+				System.out.println("buriburi");
+			}
+		}
+	}
+
+}
